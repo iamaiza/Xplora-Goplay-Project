686012e4a0b89cc1a62726e46c6234db2fee6dd4
iamaiza <aiza9382@gmail.com>
2023-06-16 15:49:08 +0500

    login updated

--- client/src/components/Auth/LoginForm.jsx
+++ client/src/components/Auth/LoginForm.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     return (
         <div className="main-form w-full h-full pt-32 overflow-x-hidden mb-5">
             <div className="mx-10 login-form-container">
-                <h2 className="text-xl leading-none min-(1025px):text-2xl ml-5">Log in</h2>
+                <h2 className="text-xl leading-none min-[1025px]:text-2xl ml-5">Log in</h2>
                 <div className="span-container mt-10 py-2 h-10 min-(1025px):h-11 text-base min-(1025px):text-xl bg-[#ffc6bb] w-fit rounded-full cursor-default">
                     <span
                         className={`h-full py-2.5 px-7 rounded-full text-color ${
