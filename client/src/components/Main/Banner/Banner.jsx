4834407d39e3760fd88f72d65e8b4cfd98bce3c4
iamaiza <aiza9382@gmail.com>
2023-06-17 18:02:41 +0500

    main content updated

--- client/src/components/Main/Banner/Banner.jsx
+++ client/src/components/Main/Banner/Banner.jsx
@@ -3,8 +3,8 @@ import "./Banner.css";
 
 const Banner = () => {
 
-    const bannerContentQueries = "max-[767px]:p-5 max-[767px]:text-center max-[550px]:mt-24 max-[1480px]:mt-0 max-[1179px]:-mt-5 max-[970px]:mt-0 max-[970px]:pl-7"
-    const headingQueries = "max-[550px]:text-[25px] max-[767px]:w-full max-[1480px]:text-5xl max-[1179px]:text-[35px] max-[970px]:text-[30px]"
+    const bannerContentQueries = "max-[600px]:p-5 max-[600px]:text-center max-[550px]:mt-24 max-[1480px]:mt-0 max-[1179px]:-mt-5 max-[970px]:mt-0 max-[970px]:pl-7"
+    const headingQueries = "max-[550px]:text-[25px] max-[600px]:w-full max-[1480px]:text-5xl max-[1179px]:text-[35px] max-[970px]:text-[30px]"
     return (
         <section className="banner-section mt-24 max-[767px]:mt-16">
             <div className="banner-img w-full flex justify-center items-center">
@@ -16,7 +16,7 @@ const Banner = () => {
                     <p className="text-xl my-4 text-margin max-[550px]:text-[16px] max-[550px]:my-4">
                         Motivate and engage your kids!
                     </p>
-                    <p className="text-xl w-[45%] max-[767px]:w-full max-[970px]:text-[16px] max-[550px]:my-4">
+                    <p className="text-xl w-[45%] max-[600px]:w-full max-[970px]:text-[16px] max-[550px]:my-4">
                         On average Xplora-kids walk over 2500 more steps
                         than others each day. It's made for kids and loved
                         by parents!
