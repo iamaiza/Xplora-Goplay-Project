3d7eff19b806904012213ef1792b52ad7f5a712e
iamaiza <aiza9382@gmail.com>
2023-06-17 17:45:14 +0500

    login updated

--- client/src/pages/Authentication/Login.jsx
+++ client/src/pages/Authentication/Login.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import './Login.css'
 import LoginForm from "../../components/Auth/LoginForm";
+import Logo from "../../UI/Logo";
 
 
 const Login = () => {
@@ -9,15 +10,7 @@ const Login = () => {
       <div className="login-page h-screen">
         <div className="login-container h-full flex gap-x-7 relative">
             <div className="flex items-center justify-between absolute top-5 left-2 sm:left-5 right-2 sm:right-5">
-              <div className="flex items-center justify-between gap-2 sm:gap-4">
-                <Link to="/" className="w-10 h-10 sm:w-14 sm:h-14 rounded-sm overflow-hidden">
-                <img className="w-full h-full object-cover" src="https://goplay.myxplora.com/_nuxt/img/xplora-logo.98b33b7.png" alt="" />
-              </Link>
-              <h1 className="text-xl block sm:text-4xl text-white">
-                Xplora
-              </h1>
-
-              </div>
+              <Logo />
               <div className="text-base sm:text-xl text-end mt-2 sm:mt-0 text-white md:text-[#7a86a1]">
               <span>New User?</span>
               <Link to="/signup" className="text-[#ff0000] md:text-[#6149cd] underline">Sign up</Link>
