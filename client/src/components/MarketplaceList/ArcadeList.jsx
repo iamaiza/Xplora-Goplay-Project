a6d39f04eb0ccb8f6cc662f73e4bf130b4ba054f
iamaiza <aiza9382@gmail.com>
2023-07-14 19:32:36 +0500

    marketplaceList added

---
+++ client/src/components/MarketplaceList/ArcadeList.jsx
@@ -0,0 +1,193 @@
+import React from "react";
+import Cards from "../../UI/Marketplace/Cards";
+
+const ArcadeList = () => {
+    return (
+        <div className="tw-mt-7">
+            <div className="tw-grid tw-grid-cols-3 max-xl:tw-grid-cols-2 max-lg:tw-grid-cols-1 tw-gap-6">
+                <Cards
+                    className="tw-h-72 max-sm:tw-w-72 max-[400px]:tw-w-full"
+                    imgSrc="https://xplora-images.s3.eu-central-1.amazonaws.com/grr.jpg"
+                    title="Giant Rabbit Run"
+                    coins="1"
+                    btnText="Play now"
+                    marketplaceList={true}
+                    cardWidth={true}
+                />
+                <Cards
+                    className="tw-h-72 max-sm:tw-w-72 max-[400px]:tw-w-full"
+                    imgSrc="https://xplora-images.s3.eu-central-1.amazonaws.com/cr.jpg"
+                    title="Curvy Road"
+                    coins="1"
+                    btnText="Play now"
+                    marketplaceList={true}
+                    cardWidth={true}
+                />
+                <Cards
+                    className="tw-h-72 max-sm:tw-w-72 max-[400px]:tw-w-full"
+                    imgSrc="https://xplora-images.s3.eu-central-1.amazonaws.com/lma.jpg"
+                    title="Leave me alone"
+                    coins="1"
+                    btnText="Play now"
+                    marketplaceList={true}
+                    cardWidth={true}
+                />
+                <Cards
+                    className="tw-h-72 max-sm:tw-w-72 max-[400px]:tw-w-full"
+                    imgSrc="https://xplora-images.s3.eu-central-1.amazonaws.com/1a30698b08e2711eca341f5c046853bbf.png"
+                    title="Slip N Slide Party"
+                    coins="1"
+                    btnText="Play now"
+                    marketplaceList={true}
+                    cardWidth={true}
+                />
+                <Cards
+                    className="tw-h-72 max-sm:tw-w-72 max-[400px]:tw-w-full"
+                    imgSrc="https://xplora-images.s3.eu-central-1.amazonaws.com/19d695f008e2711eca341f5c046853bbf.jpg"
+                    title="Cross The Bridge"
+                    coins="1"
+                    btnText="Play now"
+                    marketplaceList={true}
+                    cardWidth={true}
+                />
+                <Cards
+                    className="tw-h-72 max-sm:tw-w-72 max-[400px]:tw-w-full"
+                    imgSrc="https://xplora-images.s3.eu-central-1.amazonaws.com/sh.png"
+                    title="Skateboard Hero"
+                    coins="1"
+                    btnText="Play now"
+                    marketplaceList={true}
+                    cardWidth={true}
+                />
+                <Cards
+                    className="tw-h-72 max-sm:tw-w-72 max-[400px]:tw-w-full"
+                    imgSrc="https://xplora-images.s3.eu-central-1.amazonaws.com/pp.jpg"
+                    title="Pocket Pac"
+                    coins="1"
+                    btnText="Play now"
+                    marketplaceList={true}
+                    cardWidth={true}
+                />
+                <Cards
+                    className="tw-h-72 max-sm:tw-w-72 max-[400px]:tw-w-full"
+                    imgSrc="https://xplora-images.s3.eu-central-1.amazonaws.com/as.png"
+                    title="Airplane Survival"
+                    coins="1"
+                    btnText="Play now"
+                    marketplaceList={true}
+                    cardWidth={true}
+                />
+                <Cards
+                    className="tw-h-72 max-sm:tw-w-72 max-[400px]:tw-w-full"
+                    imgSrc="https://xplora-images.s3.eu-central-1.amazonaws.com/dtmu.png"
+                    title="Don't Mess Up"
+                    coins="1"
+                    btnText="Play now"
+                    marketplaceList={true}
+                    cardWidth={true}
+                />
+                <Cards
+                    className="tw-h-72 max-sm:tw-w-72 max-[400px]:tw-w-full"
+                    imgSrc="https://xplora-images.s3.eu-central-1.amazonaws.com/br.png"
+                    title="Banana Running"
+                    coins="1"
+                    btnText="Play now"
+                    marketplaceList={true}
+                    cardWidth={true}
+                />
+                <Cards
+                    className="tw-h-72 max-sm:tw-w-72 max-[400px]:tw-w-full"
+                    imgSrc="https://xplora-images.s3.eu-central-1.amazonaws.com/ms.jpg"
+                    title="Mine Sweeper"
+                    coins="1"
+                    btnText="Play now"
+                    marketplaceList={true}
+                    cardWidth={true}
+                />
+                <Cards
+                    className="tw-h-72 max-sm:tw-w-72 max-[400px]:tw-w-full"
+                    imgSrc="https://xplora-images.s3.eu-central-1.amazonaws.com/ezm.jpg"
+                    title="EZ Mahjong"
+                    coins="1"
+                    btnText="Play now"
+                    marketplaceList={true}
+                    cardWidth={true}
+                />
+                <Cards
+                    className="tw-h-72 max-sm:tw-w-72 max-[400px]:tw-w-full"
+                    imgSrc="https://xplora-images.s3.eu-central-1.amazonaws.com/cj.jpg"
+                    title="Crazy Jetpack"
+                    coins="1"
+                    btnText="Play now"
+                    marketplaceList={true}
+                    cardWidth={true}
+                />
+                <Cards
+                    className="tw-h-72 max-sm:tw-w-72 max-[400px]:tw-w-full"
+                    imgSrc="https://xplora-images.s3.eu-central-1.amazonaws.com/bb.png"
+                    title="Brick Breaker"
+                    coins="1"
+                    btnText="Play now"
+                    marketplaceList={true}
+                    cardWidth={true}
+                />
+                <Cards
+                    className="tw-h-72 max-sm:tw-w-72 max-[400px]:tw-w-full"
+                    imgSrc="https://xplora-images.s3.eu-central-1.amazonaws.com/1d01b87608d8311ecb1d6ef826ea3bc7f.png"
+                    title="Alpha Space Invasion"
+                    coins="1"
+                    btnText="Play now"
+                    marketplaceList={true}
+                    cardWidth={true}
+                />
+                <Cards
+                    className="tw-h-72 max-sm:tw-w-72 max-[400px]:tw-w-full"
+                    imgSrc="https://xplora-images.s3.eu-central-1.amazonaws.com/sta.jpg"
+                    title="Shoot The Asteroids"
+                    coins="1"
+                    btnText="Play now"
+                    marketplaceList={true}
+                    cardWidth={true}
+                />
+                <Cards
+                    className="tw-h-72 max-sm:tw-w-72 max-[400px]:tw-w-full"
+                    imgSrc="https://xplora-images.s3.eu-central-1.amazonaws.com/nc.jpg"
+                    title="Numbers Challenge"
+                    coins="1"
+                    btnText="Play now"
+                    marketplaceList={true}
+                    cardWidth={true}
+                />
+                <Cards
+                    className="tw-h-72 max-sm:tw-w-72 max-[400px]:tw-w-full"
+                    imgSrc="https://xplora-images.s3.eu-central-1.amazonaws.com/1a4440f60826e11ec93e3e90d803d5821.png"
+                    title="Defend the castle"
+                    coins="1"
+                    btnText="Play now"
+                    marketplaceList={true}
+                    cardWidth={true}
+                />
+                <Cards
+                    className="tw-h-72 max-sm:tw-w-72 max-[400px]:tw-w-full"
+                    imgSrc="https://xplora-images.s3.eu-central-1.amazonaws.com/153e8da608b4b11ecbb58ef20ac4889aa.jpeg"
+                    title="Trixology Classic"
+                    coins="1"
+                    btnText="Play now"
+                    marketplaceList={true}
+                    cardWidth={true}
+                />
+                <Cards
+                    className="tw-h-72 max-sm:tw-w-72 max-[400px]:tw-w-full"
+                    imgSrc="https://xplora-images.s3.eu-central-1.amazonaws.com/bv.png"
+                    title="Beach Volley"
+                    coins="1"
+                    btnText="Play now"
+                    marketplaceList={true}
+                    cardWidth={true}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default ArcadeList;
---
+++ client/src/components/MarketplaceList/WatchFaceList.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+import Cards from "../../UI/Marketplace/Cards";
+
+const WatchFaceList = () => {
+    return (
+        <div className="tw-mt-7">
+            <div className="tw-grid tw-grid-cols-4 max-xl:tw-grid-cols-3 max-lg:tw-grid-cols-2 max-sm:tw-grid-cols-1 tw-gap-6">
+                <Cards
+                    className="tw-h-[17rem] tw-relative max-sm:tw-w-72 max-[400px]:tw-w-full tw-mx-auto"
+                    imgSrc="https://goplay.myxplora.com/_nuxt/img/watch-frame.4cc16e8.png"
+                    imgSrc2="https://xplora-images.s3.eu-central-1.amazonaws.com/GO-WF-007.png"
+                    title="Nice Breeze"
+                    coins="30"
+                    btnText="Get"
+                    tag={true}
+                    marketplaceList={true}
+                />
+
+                <Cards
+                    className="tw-h-[17rem] tw-relative max-sm:tw-w-72 max-[400px]:tw-w-full tw-mx-auto"
+                    imgSrc="https://goplay.myxplora.com/_nuxt/img/watch-frame.4cc16e8.png"
+                    imgSrc2="https://xplora-images.s3.eu-central-1.amazonaws.com/GO-WF-008.png"
+                    title="Sunnyside"
+                    coins="30"
+                    btnText="Get"
+                    tag={true}
+                    marketplaceList={true}
+                />
+
+                <Cards
+                    className="tw-h-[17rem] tw-relative max-sm:tw-w-72 max-[400px]:tw-w-full tw-mx-auto"
+                    imgSrc="https://goplay.myxplora.com/_nuxt/img/watch-frame.4cc16e8.png"
+                    imgSrc2="https://xplora-images.s3.eu-central-1.amazonaws.com/GO-WF-009.png"
+                    title="Pinky"
+                    coins="30"
+                    btnText="Get"
+                    tag={true}
+                    marketplaceList={true}
+                />
+                <Cards
+                    className="tw-h-[17rem] tw-relative max-sm:tw-w-72 max-[400px]:tw-w-full tw-mx-auto"
+                    imgSrc="https://goplay.myxplora.com/_nuxt/img/watch-frame.4cc16e8.png"
+                    imgSrc2="https://xplora-images.s3.eu-central-1.amazonaws.com/watch-face/1b76551b01fe011ee83ce55302323b59c.png"
+                    title="Green galore"
+                    coins="30"
+                    btnText="Get"
+                    tag={true}
+                    marketplaceList={true}
+                />
+                <Cards
+                    className="tw-h-[17rem] tw-relative max-sm:tw-w-72 max-[400px]:tw-w-full tw-mx-auto"
+                    imgSrc="https://goplay.myxplora.com/_nuxt/img/watch-frame.4cc16e8.png"
+                    imgSrc2="https://xplora-images.s3.eu-central-1.amazonaws.com/watch-face/1ba7243901fe011ee83ce55302323b59c.png"
+                    title="Cupcake party"
+                    coins="30"
+                    btnText="Get"
+                    tag={true}
+                    marketplaceList={true}
+                />
+                <Cards
+                    className="tw-h-[17rem] tw-relative max-sm:tw-w-72 max-[400px]:tw-w-full tw-mx-auto"
+                    imgSrc="https://goplay.myxplora.com/_nuxt/img/watch-frame.4cc16e8.png"
+                    imgSrc2="https://xplora-images.s3.eu-central-1.amazonaws.com/watch-face/1bc94d5701fe011ee83ce55302323b59c.png"
+                    title="Love cake"
+                    coins="30"
+                    btnText="Get"
+                    tag={true}
+                    marketplaceList={true}
+                />
+                <Cards
+                    className="tw-h-[17rem] tw-relative max-sm:tw-w-72 max-[400px]:tw-w-full tw-mx-auto"
+                    imgSrc="https://goplay.myxplora.com/_nuxt/img/watch-frame.4cc16e8.png"
+                    imgSrc2="https://xplora-images.s3.eu-central-1.amazonaws.com/watch-face/1bd7041501fe011ee83ce55302323b59c.png"
+                    title="Green galore"
+                    coins="30"
+                    btnText="Get"
+                    tag={true}
+                    marketplaceList={true}
+                />
+                <Cards
+                    className="tw-h-[17rem] tw-relative max-sm:tw-w-72 max-[400px]:tw-w-full tw-mx-auto"
+                    imgSrc="https://goplay.myxplora.com/_nuxt/img/watch-frame.4cc16e8.png"
+                    imgSrc2="https://xplora-images.s3.eu-central-1.amazonaws.com/watch-face/1d005f2101fe011ee83ce55302323b59c.png"
+                    title="Fish shell"
+                    coins="30"
+                    btnText="Get"
+                    tag={true}
+                    marketplaceList={true}
+                />
+                <Cards
+                    className="tw-h-[17rem] tw-relative max-sm:tw-w-72 max-[400px]:tw-w-full tw-mx-auto"
+                    imgSrc="https://goplay.myxplora.com/_nuxt/img/watch-frame.4cc16e8.png"
+                    imgSrc2="https://xplora-images.s3.eu-central-1.amazonaws.com/watch-face/1d650a9d01fe011ee83ce55302323b59c.png"
+                    title="Creative pink"
+                    coins="30"
+                    btnText="Get"
+                    tag={true}
+                    marketplaceList={true}
+                />
+                <Cards
+                    className="tw-h-[17rem] tw-relative max-sm:tw-w-72 max-[400px]:tw-w-full tw-mx-auto"
+                    imgSrc="https://goplay.myxplora.com/_nuxt/img/watch-frame.4cc16e8.png"
+                    imgSrc2="https://xplora-images.s3.eu-central-1.amazonaws.com/watch-face/1de90f0001fe011ee83ce55302323b59c.png"
+                    title="Dizzy spiral"
+                    coins="30"
+                    btnText="Get"
+                    tag={true}
+                    marketplaceList={true}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default WatchFaceList;
