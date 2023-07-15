6c57326d0d5b96295957039df8e2927adf400b84
iamaiza <aiza9382@gmail.com>
2023-07-15 12:56:41 +0500

    header, main content updated

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,11 +5,11 @@ import Logo from "../../UI/MainUI/Logo";
 
 const Header = () => {
     return (
-        <header className="header-bg tw-px-3 tw-py-1 sm:tw-py-3 tw-fixed tw-top-0 tw-left-0 tw-right-0 tw-z-20 ">
+        <header className="header-bg tw-p-3 max-lg:tw-py-1 tw-fixed tw-top-0 tw-left-0 tw-right-0 tw-z-20 ">
             <div className="tw-flex tw-justify-between tw-items-center tw-gap-3">
-                <Logo classname='max-[1265px]:tw-hidden' logosize='tw-w-8 tw-h-8 sm:tw-w-11 sm:tw-h-11' />
+                <Logo classname='max-[1265px]:tw-hidden' logosize='tw-w-11 tw-h-11 max-lg:tw-w-9 max-lg:tw-h-9' />
                 <div className="tw-flex tw-items-center tw-gap-9">
-                    <Link to="/login" className="tw-border tw-text-white tw-px-6 tw-py-3 sm:tw-px-7 sm:tw-py-4 tw-rounded-lg tw-text-sm hover:tw-bg-white hover:tw-bg-opacity-10">Login</Link>
+                    <Link to="/login" className="tw-border tw-text-white tw-px-7 tw-py-4 max-lg:tw-py-3 tw-rounded-lg tw-text-sm hover:tw-bg-white hover:tw-bg-opacity-10">Login</Link>
                 </div>
             </div>
         </header>
