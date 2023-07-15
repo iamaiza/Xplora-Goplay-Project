1e4b8b86452b0b929b10a3b11841123271319a04
iamaiza <88074799+iamaiza@users.noreply.github.com>
2023-07-15 10:48:43 +0500

    file updated

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,8 @@
 export { default as LandingPage } from "./pages/MainPages/LandingPage"
 export { default as Login } from "./pages/Authentication/Login"
 export { default as SignUp } from "./pages/Authentication/SignUp"
-<<<<<<< Updated upstream
 export { default as Welcome } from "./pages/MainPages/WelcomePage"
 export { default as Dashboard } from "./pages/Dashboard/Dashboard"
-=======
-<<<<<<< Updated upstream
-=======
 export { default as Welcome } from "./pages/MainPages/WelcomePage"
 export { default as Dashboard } from "./pages/Dashboard/Dashboard"
 export { default as Adventures } from "./pages/Adventures/Adventures"
@@ -15,5 +11,3 @@ export { default as AuctionPage } from "./pages/MarketplaceList/AuctionPage"
 export { default as ArcadePage } from "./pages/MarketplaceList/ArcadePage"
 export { default as WatchFacePage } from './pages/MarketplaceList/WatchFacePage'
 export { default as WatchRingtonesPage } from './pages/MarketplaceList/WatchRingtonesPage'
->>>>>>> Stashed changes
->>>>>>> Stashed changes
