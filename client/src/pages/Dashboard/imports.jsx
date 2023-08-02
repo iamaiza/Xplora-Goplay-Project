ced14c3465c48073c61d7d59f9b1f58b893b88c3
iamaiza <aiza9382@gmail.com>
2023-08-02 11:56:32 +0500

    dashboard new commit

--- client/src/pages/Dashboard/imports.jsx
+++
@@ -1,4 +0,0 @@
-export { default as DashboardHeader} from "../../UI/MainUI/DashboardHeader";
-export { default as DashboardSidebar } from "../../UI/MainUI/DashboardSidebar";
-export { default as DashboardGridCol1 } from "../../components/Dashboard/DashboardGridCol1";
-export { default as DashboardGridCol2 } from "../../components/Dashboard/DashboardGridCol2";
